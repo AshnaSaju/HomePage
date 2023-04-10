9f3ec4f5e6ecd58b8d15e9ce216f08f858325f60
AshnaSaju <126075672+AshnaSaju@users.noreply.github.com>
2023-04-10 11:32:58 +0530

    Update CreateModal.jsx

--- Home/src/Components/Modals/CreateModal/CreateModal.jsx
+++ Home/src/Components/Modals/CreateModal/CreateModal.jsx
@@ -1,11 +1,28 @@
-import React, { useContext } from 'react'
+import React, { useContext,useState } from 'react'
 import ModalContext from '../../../Context/ModalContext'
 import "./createmodal.css"
+import { Web3Storage } from 'web3.storage'
 
 const CreateModal = () => {
     const context = useContext(ModalContext)
     const { createModal, closeCreateModal } = context
+    const [ipfsFile,setipfsFile]=useState(null)
+    function getAccessToken () {
+  return "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkaWQ6ZXRocjoweDA5MzQzQzUzOTM2MTE4NTNCZDNiNjY3RjZjNWQwZTkzOTkzMzhDQjEiLCJpc3MiOiJ3ZWIzLXN0b3JhZ2UiLCJpYXQiOjE2ODEwMzU4ODY1MTcsIm5hbWUiOiJ0b2tlbjEifQ.BwE_yRAZijnjQ-_RvgB6entVfS0gRboSxCkowK7DgsU"
+}
 
+function makeStorageClient () {
+  return new Web3Storage({ token: getAccessToken() })
+}
+function getFiles (e) {
+  const fileInput = e.target.files
+  setipfsFile(fileInput)
+}
+    async function storeFiles (files) {
+  const client = makeStorageClient()
+  const cid = await client.put(files)
+  console.log('stored files with cid:', cid)
+}
     return (
         <>
             {createModal && <div className='CreateModal'>
@@ -19,11 +36,12 @@ const CreateModal = () => {
                     <div className="body">
                         <i style={{ fontSize: "7em" }} class='bx bx-images'></i>
                         <p style={{ margin: "2%" }}>Upload Your Images Here</p>
-                        <input type="file" id="file" className='btn' />
+                        <input onChange={getFiles} type="file" id="file" className='btn' />
                         <label for="file">
                             Select from Computer
                         </label>
                     </div>
+                <button onClick={()=>storeFiles(ipfsFile)}>submit</button>
                 </div>
             </div>
 
